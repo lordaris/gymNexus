7872a8fb015982c1d67d1f95e6f6b4c6f5361f4a
Jesús Armando Peña Lucero <audio.aris@gmail.com>
2023-05-27 09:26:39 -0600

    toasts

--- frontend/src/pages/coach/dashboard/workouts/[id].js
+++ frontend/src/pages/coach/dashboard/workouts/[id].js
@@ -6,9 +6,7 @@ import Layout from "../../../components/ui/coachLayout";
 import Link from "next/link";
 import { BsTrashFill } from "react-icons/bs";
 import { FaEdit } from "react-icons/fa";
-import handleDeleteDay from "../../../components/handleDeleteDay";
-import handleDeleteExercise from "../../../components/handleDeleteExercise";
-import handleDeleteWorkout from "../../../components/handleDeleteWorkout";
+import { toast } from "react-toastify";
 
 function Workouts() {
   const [workout, setWorkout] = useState(null);
@@ -30,6 +28,101 @@ function Workouts() {
   if (!workout) {
     return <div>Loading...</div>;
   }
+
+  const handleDeleteDay = (workoutId, dayId, setWorkout, token) => {
+    if (window.confirm("Are you sure you want to delete this day?")) {
+      axios
+        .delete(
+          process.env.NEXT_PUBLIC_API_URL +
+            `/workouts/delete/${workoutId}/${dayId}`,
+          {
+            headers: { Authorization: `${token}` },
+          }
+        )
+        .then((response) => {
+          toast.success("Day deleted successfully", {
+            position: "bottom-center",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          }); // Update the state of the workout to reflect the deletion
+          setWorkout((prevState) => {
+            const updatedDays = prevState.days.filter(
+              (day) => day._id !== dayId
+            );
+            return { ...prevState, days: updatedDays };
+          });
+        })
+        .catch((error) => console.error(error));
+    }
+  };
+  const handleDeleteWorkout = (id, token, router) => {
+    if (window.confirm("¿Está seguro de que desea eliminar esta rutina?")) {
+      axios
+        .delete(process.env.NEXT_PUBLIC_API_URL + `/workouts/delete/${id}`, {
+          headers: { Authorization: `${token}` },
+        })
+        .then((response) => {
+          toast.success("Workout has been successfully deleted", {
+            position: "bottom-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+          router.push("/coach/dashboard/workouts");
+        })
+        .catch((error) => console.error(error));
+    }
+  };
+  const handleDeleteExercise = (
+    workoutId,
+    dayId,
+    exerciseId,
+    setWorkout,
+    token
+  ) => {
+    if (window.confirm("Are you sure you want to delete this exercise?")) {
+      axios
+        .delete(
+          process.env.NEXT_PUBLIC_API_URL +
+            `/workouts/delete/${workoutId}/${dayId}/${exerciseId}`,
+          { headers: { Authorization: `${token}` } }
+        )
+        .then((response) => {
+          toast.success("exercise deleted successfully", {
+            position: "bottom-center",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          }); // Update the state of the workout to reflect the deletion
+          setWorkout((prevState) => {
+            const updatedDays = prevState.days.map((day) => {
+              if (day._id === dayId) {
+                const updatedExercises = day.exercises.filter(
+                  (exercise) => exercise._id !== exerciseId
+                );
+                return { ...day, exercises: updatedExercises };
+              }
+              return day;
+            });
+            return { ...prevState, days: updatedDays };
+          });
+        })
+        .catch((error) => console.error(error));
+    }
+  };
 
   const toggleDetails = (day) => {
     if (selectedDay === day) {
